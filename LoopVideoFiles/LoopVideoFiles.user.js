0ba35e81042e1fb81c23e8ddf38474ccbf9d9dd3
var512 <var512dev@gmail.com>
2019-07-18 17:45:18 -0300

    LoopVideoFiles: Update script to follow ESLint rules and release version 1.0.0

--- LoopVideoFiles/LoopVideoFiles.user.js
+++ LoopVideoFiles/LoopVideoFiles.user.js
@@ -3,7 +3,7 @@
 // @description  Sets the loop attribute when viewing video files in their own window/tab.
 // @namespace    var512
 // @author       var512
-// @version      0.0.2
+// @version      1.0.0
 // @supportURL   https://gitlab.com/var512
 // @supportURL   https://github.com/var512
 // @include      /\.(m4v|mp4|ogv|webm)$/
@@ -14,12 +14,10 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function () {
-    "use strict";
+(() => {
+  const elements = document.getElementsByTagName('video');
 
-    const elements = document.getElementsByTagName("video");
-
-    if (elements.item(0) !== null) {
-        elements.item(0).setAttribute("loop", "true");
-    }
+  if (elements.item(0) !== null) {
+    elements.item(0).setAttribute('loop', 'true');
+  }
 })();
